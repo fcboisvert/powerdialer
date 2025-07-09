38d5c7754a9d0b09d708377707aa3edf0d4f9e5b
Frédéric-Charles Boisvert <fcboisvert@texion.ca>
2025-07-09 17:47:46 -0400

    style: switch to pure Tailwind index.css (no starter overrides)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+// src/main.tsx
+ import React from 'react'
+ import ReactDOM from 'react-dom/client'
+ import './index.css'          // 👈  add this line back
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ import App from './App.tsx'
+ 
+ ReactDOM.createRoot(document.getElementById('root')!).render(
+   <React.StrictMode>
+     <App />
+   </React.StrictMode>,
+ )
