528a40344388cbd57c8e08b702fdccad58ce10d6
Frédéric-Charles Boisvert <fcboisvert@texion.ca>
2025-07-09 17:12:53 -0400

    fix(build): tweak configs and deps for Cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
-  define: {
-    global: "globalThis",
-  }
+  resolve: { alias: { "@": "/src" } },
+  define: { global: "globalThis" }
 });
